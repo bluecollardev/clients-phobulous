ab86bb84feacb95b8154eff579a741eb4118d4e6
Lucas <tech@firebrandwebsolutions.com>
2019-05-02 02:37:38 -0700

    Update config.

---
+++ upload/admin/config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8081/admin/');
+define('HTTP_CATALOG', 'http://localhost:8081/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8081/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8081/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/upload/admin/');
+define('DIR_SYSTEM', '/var/www/upload/system/');
+define('DIR_LANGUAGE', '/var/www/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/upload/system/config/');
+define('DIR_IMAGE', '/var/www/upload/image/');
+define('DIR_CACHE', '/var/www/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/upload/system/storage/download/');
+define('DIR_UPLOAD', '/var/www/upload/system/storage/upload/');
+define('DIR_LOGS', '/var/www/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/upload/system/storage/modification/');
+define('DIR_CATALOG', '/var/www/upload/catalog/');
+define('DIR_QC', '/var/www/');
+define('QUICKCOMMERCE_INSTALLED', true);
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'quickcommerce');
+define('DB_PASSWORD', 'quickcommerce');
+define('DB_DATABASE', 'quickcommerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc2_');
---
+++ upload/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8081/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8081/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/upload/system/');
+define('DIR_LANGUAGE', '/var/www/upload/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/upload/system/config/');
+define('DIR_IMAGE', '/var/www/upload/image/');
+define('DIR_CACHE', '/var/www/upload/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/upload/system/download/');
+define('DIR_UPLOAD', '/var/www/upload/system/upload/');
+define('DIR_MODIFICATION', '/var/www/upload/system/modification/');
+define('DIR_LOGS', '/var/www/upload/system/logs/');
+define('DIR_QC', '/var/www/');
+define('QUICKCOMMERCE_INSTALLED', true);
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'quickcommerce');
+define('DB_PASSWORD', 'quickcommerce');
+define('DB_DATABASE', 'quickcommerce');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc2_');
